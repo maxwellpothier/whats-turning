36c93e50db0061b8701c6970a88132b3b9969a4a
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-14 15:46:21 -0700

    fixes some build errors

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,16 @@ import Script from "next/script";
 const MyApp = ({Component, pageProps}) => {
 	return (
 		<>
-		<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-DZE177VCFL"/>
+		<Script strategy={"afterInteractive"} src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}/>
 		<Script
-			id='google-analytics'
-			strategy="afterInteractive"
+			id={'google-analytics'}
+			strategy={"afterInteractive"}
 			dangerouslySetInnerHTML={{
 				__html: `
 				window.dataLayer = window.dataLayer || [];
 				function gtag(){dataLayer.push(arguments);}
 				gtag('js', new Date());
-				gtag('config', 'G-XXXXXXX', {
+				gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
 				page_path: window.location.pathname,
 				});
 				`,
