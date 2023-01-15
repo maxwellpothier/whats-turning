9dd3cb281653216569ec5e14f1279aa6938a1509
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-14 17:22:30 -0700

    shifting around some code

--- pages/_app.js
+++ pages/_app.js
@@ -8,23 +8,23 @@ import Script from "next/script";
 const MyApp = ({Component, pageProps}) => {
 	return (
 		<>
-		<Script strategy={"afterInteractive"} src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}/>
-		<Script
-			id={'google-analytics'}
-			strategy={"afterInteractive"}
-			dangerouslySetInnerHTML={{
-				__html: `
-				window.dataLayer = window.dataLayer || [];
-				function gtag(){dataLayer.push(arguments);}
-				gtag('js', new Date());
-				gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-				page_path: window.location.pathname,
-				});
-				`,
-			}}
-		/>
 
 			<Component {...pageProps}/>
+			<Script strategy={"afterInteractive"} src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}/>
+			<Script
+				id={'google-analytics'}
+				strategy={"afterInteractive"}
+				dangerouslySetInnerHTML={{
+					__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
+						page_path: window.location.pathname,
+						});
+					`,
+				}}
+			/>
 			<ToastContainer/>
 		</>
 	);
