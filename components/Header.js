a78f011da7efb9a7d37bf89086a8936af2aee179
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-15 17:16:55 -0700

    implements max-width on the header

--- components/Header.js
+++ components/Header.js
@@ -6,19 +6,21 @@ import WTButton from "./WTButton";
 
 const Header = () => {
 	return (
-        <div className={styles.headerContainer}>
-			<Link href={"/"} passHref>
-				<WTLogo/>
-			</Link>
-			<div className={styles.hamburgerMenu}>
-				<div className={styles.menuLink}/>
-				<div className={styles.menuLink}/>
-				<div className={styles.menuLink}/>
+		<div className={styles.background}>
+			<div className={styles.headerContainer}>
+				<Link href={"/"} passHref>
+					<WTLogo/>
+				</Link>
+				<div className={styles.hamburgerMenu}>
+					<div className={styles.menuLink}/>
+					<div className={styles.menuLink}/>
+					<div className={styles.menuLink}/>
+				</div>
+				<div className={styles.headerLinks}>
+					<WTButton content={"Join the Waitlist"}/>
+				</div>
 			</div>
-			<div className={styles.headerLinks}>
-				<WTButton content={"Join the Waitlist"}/>
-			</div>
-        </div>
+		</div>
     );
 };
 
