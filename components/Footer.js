c618003e60a0f7698b6daead660cbdad7fc2ab08
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-15 17:11:16 -0700

    shrinks the max footer width

--- components/Footer.js
+++ components/Footer.js
@@ -4,26 +4,28 @@ import {FaSpotify, FaGithub, FaTwitter} from "react-icons/fa";
 
 const Footer = () => {
 	return (
-		<div className={styles.footerContainer}>
-			<div className={styles.contactSection}>
-				<WTMobileLogo className={styles.footerLogo}/>
-				<div>
-					<span className={styles.contactText}>Contact Us!</span>
-					<a className={styles.emailLink}>max@whatsturning.com</a>
+		<div className={styles.background}>
+			<div className={styles.footerContainer}>
+				<div className={styles.contactSection}>
+					<WTMobileLogo className={styles.footerLogo}/>
+					<div>
+						<span className={styles.contactText}>Contact Us!</span>
+						<a className={styles.emailLink}>max@whatsturning.com</a>
+					</div>
+					<div className={styles.contactText}>We would love to hear any feedback!</div>
 				</div>
-				<div className={styles.contactText}>We would love to hear any feedback!</div>
+				<ul className={styles.socialLinks}>
+					<a href="https://open.spotify.com/user/bg8527p8wb1nrpb6r8xf662r2?si=a32cb6c1e3214d3a" target={"_blank"} rel={"noreferrer"}>
+						<FaSpotify className={styles.footerIcon}/>
+					</a>
+					<a href="https://github.com/maxwellpothier/whats-turning/tree/main" target={"_blank"} rel={"noreferrer"}>
+						<FaGithub className={styles.footerIcon}/>
+					</a>
+					<a href="https://twitter.com/MaxPothier" target={"_blank"} rel={"noreferrer"}>
+						<FaTwitter className={styles.footerIcon}/>
+					</a>
+				</ul>
 			</div>
-            <ul className={styles.socialLinks}>
-                <a href="https://open.spotify.com/user/bg8527p8wb1nrpb6r8xf662r2?si=a32cb6c1e3214d3a" target={"_blank"} rel={"noreferrer"}>
-                    <FaSpotify className={styles.footerIcon}/>
-                </a>
-                <a href="https://github.com/maxwellpothier/whats-turning/tree/main" target={"_blank"} rel={"noreferrer"}>
-                    <FaGithub className={styles.footerIcon}/>
-                </a>
-                <a href="https://twitter.com/MaxPothier" target={"_blank"} rel={"noreferrer"}>
-                    <FaTwitter className={styles.footerIcon}/>
-                </a>
-            </ul>
 		</div>
 	);
 };
