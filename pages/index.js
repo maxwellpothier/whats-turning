d2a255be3e35ce6f7969d1de357ba65f7006cb76
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-20 20:51:39 -0700

    beginnings of the final section

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,11 @@ const Home = () => {
 				<PostCard/>
 			</div>
 			<HorizontalLine/>
+			<div className={styles.productDescription}>
+				<div>
+				We aren&apos;t quite finished yet, but here&apos;s how it&apos;ll work:
+				</div>
+			</div>
 		</Theme>
 	);
 };
