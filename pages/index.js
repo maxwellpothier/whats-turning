35da6df94958484e3271fa41ab1ca2d62f313536
maxwellpothier <pothiermaxwell@gmail.com>
2023-01-31 19:57:32 -0700

    finishes excuse section

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Theme from "../components/theme/Theme";
 import TodaysAlbum from "../components/TodaysAlbum";
 import PostCard from "../components/post/PostCard";
 import HorizontalLine from "../components/HorizontalLine";
+import Container from "../components/theme/Container";
 
 const Home = () => {
 	return (
@@ -17,15 +18,15 @@ const Home = () => {
 				<PostCard/>
 			</div>
 			<HorizontalLine/>
-			<div className={styles.productDescription}>
+			<Container>
 				<h3 className={styles.excuse}>We aren&apos;t quite finished yet, but here&apos;s how it&apos;ll work:</h3>
-				<ul>
+				<ul className={styles.stepList}>
 					<li>Listen to the What&apos;s Turning Album of the Day</li>
 					<li>Click “Create Post”</li>
 					<li>Leave a rating and optional short review</li>
 					<li>Watch your profile grow along with your taste and understanding of music!</li>
 				</ul>
-			</div>
+			</Container>
 		</Theme>
 	);
 };
