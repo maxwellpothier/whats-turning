60ded50909ec1fec80a1f40450d83cc26907cc89
maxwellpothier <pothiermaxwell@gmail.com>
2023-03-06 20:30:53 -0700

    loader for post page

--- pages/post/[postId].js
+++ pages/post/[postId].js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react";
 import { getPostForId } from "../../utils/postUtils";
 import Theme from "../../components/theme/Theme";
 import HorizontalLine from "../../components/theme/HorizontalLine";
+import ArtWithScore from "../../components/post/ArtWithScore";
+import LoadMaster from "../../components/theme/LoadMaster";
 
 import styles from "./postId.module.scss";
-import Image from "next/image";
-import ArtWithScore from "../../components/post/ArtWithScore";
 
 const SinglePost = () => {
 	const router = useRouter();
@@ -26,31 +26,33 @@ const SinglePost = () => {
 
 	return (
 		<Theme>
-			{!isLoading &&
-				<>
-					<div className={styles.postPageHeader}>
-						<div className={styles.nameDate}>
-							<p className={styles.username}>@{post.belongsTo.username}</p>
-							<p className={styles.date}>{`${date.toLocaleString('default', { month: 'short' })} ${date.getDate()}, ${date.getFullYear()}`}</p>
-						</div>
-						<div className={styles.albumInfo}>
-							<ArtWithScore
-								album={post.album}
-								rating={post.rating}
-							/>
-							<div>
-								<p className={styles.albumTitle}>{post.album.title}</p>
-								<p className={styles.albumArtist}>{post.album.artist}</p>
-								<p className={styles.albumYear}>{post.album.yearReleased}</p>
+			<LoadMaster className={styles.loadContainer} isLoading={isLoading}>
+				{!isLoading &&
+					<>
+						<div className={styles.postPageHeader}>
+							<div className={styles.nameDate}>
+								<p className={styles.username}>@{post.belongsTo.username}</p>
+								<p className={styles.date}>{`${date.toLocaleString('default', { month: 'short' })} ${date.getDate()}, ${date.getFullYear()}`}</p>
+							</div>
+							<div className={styles.albumInfo}>
+								<ArtWithScore
+									album={post.album}
+									rating={post.rating}
+								/>
+								<div>
+									<p className={styles.albumTitle}>{post.album.title}</p>
+									<p className={styles.albumArtist}>{post.album.artist}</p>
+									<p className={styles.albumYear}>{post.album.yearReleased}</p>
+								</div>
 							</div>
 						</div>
-					</div>
-					<HorizontalLine/>
-					<div className={styles.contentContainer}>
-						<p className={styles.content}>{post.content}</p>
-					</div>
-				</>
-			}
+						<HorizontalLine/>
+						<div className={styles.contentContainer}>
+							<p className={styles.content}>{post.content}</p>
+						</div>
+					</>
+				}
+			</LoadMaster>
 		</Theme>
 	);
 };
