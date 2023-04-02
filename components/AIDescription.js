70462ce151575eca548314d68335ad99af7fdb35
maxwellpothier <pothiermaxwell@gmail.com>
2023-04-02 00:47:15 -0600

    new styling for description

--- components/AIDescription.js
+++ components/AIDescription.js
@@ -1,17 +1,17 @@
 import Container from "./theme/Container";
 
 import styles from "./aiDescription.module.scss";
-import {getAiDescription} from "../utils/albumUtils";
 
 const AIDescription = ({albumTitle, description}) => {
 	const callToOpenAI = async () => {};
 
 	return (
-		<Container>
+		<Container className={styles.descriptionWrapper}>
 			<Container>
 				<h2 className={styles.moreAboutTitle}>
 					More about {albumTitle}
 				</h2>
+				<span className={styles.aiShoutout}>Powered by Open AI</span>
 			</Container>
 			<p className={styles.moreAboutText}>{description}</p>
 		</Container>
