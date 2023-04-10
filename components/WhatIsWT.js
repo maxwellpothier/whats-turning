5b280b132a346456b49df5948c3fff645593e6b1
maxwellpothier <pothiermaxwell@gmail.com>
2023-04-09 18:22:06 -0600

    fixing some errors

--- components/WhatIsWT.js
+++ components/WhatIsWT.js
@@ -5,17 +5,17 @@ const WhatIsWT = () => {
 		<div className={styles.whatIsWTContainer}>
 			<h2 className={styles.whatIsWTHeader}>Explore. Rate. Repeat.</h2>
 			<p className={styles.whatIsWTParagraph}>
-				Welcome to "What's Turning?" - the app that helps you discover
-				new music every day! Here's how it works: Check out the "Album
-				of the Day" - we handpick a new album every day for you to
-				listen to. Listen to the album and rate it on a scale of 1 to 5
-				stars. Leave a review of the album, sharing your thoughts,
-				feelings, and opinions. Keep track of your progress and music
-				taste by viewing your personal profile. "Whats Turning?" is
-				perfect for music lovers of all levels. Whether you're a casual
-				listener, or a die-hard fan, you'll find something new and
-				exciting every day. So join us today, and discover the music
-				that's turning heads!
+				Welcome to What&apos;s Turning? - the app that helps you
+				discover new music every day! Here&apos;s how it works: Check
+				out the Album of the Day - we handpick a new album every day for
+				you to listen to. Listen to the album and rate it on a scale of
+				1 to 5 stars. Leave a review of the album, sharing your
+				thoughts, feelings, and opinions. Keep track of your progress
+				and music taste by viewing your personal profile. What&apos;s
+				Turning? is perfect for music lovers of all levels. Whether
+				you&apos;re a casual listener, or a die-hard fan, you&apos;ll
+				find something new and exciting every day. So join us today, and
+				discover the music that&apos;s turning heads!
 			</p>
 		</div>
 	);
