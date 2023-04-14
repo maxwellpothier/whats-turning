e37fd401f605249bead2b216b5b086b443abd390
maxwellpothier <pothiermaxwell@gmail.com>
2023-04-13 22:14:23 -0600

    swap numbers for icons

--- components/HowToStep.js
+++ components/HowToStep.js
@@ -1,9 +1,9 @@
 import styles from "./howToStep.module.scss";
 
-const HowToStep = ({stepNumber, stepDescription}) => {
+const HowToStep = ({icon, stepDescription}) => {
 	return (
 		<div className={styles.boxOutline}>
-			<div className={styles.stepNumber}>{stepNumber}.</div>
+			<div className={styles.stepNumber}>{icon}</div>
 			<div className={styles.description}>{stepDescription}</div>
 		</div>
 	);
--- components/WhatIsWT.js
+++ components/WhatIsWT.js
@@ -1,6 +1,8 @@
 import HowToStep from "./HowToStep";
-import PostCard from "./post/PostCard";
 import Container from "./theme/Container";
+import {SlGraph} from "react-icons/sl";
+import {CgMusicSpeaker} from "react-icons/cg";
+import {TfiWrite} from "react-icons/tfi";
 
 import styles from "./whatIsWT.module.scss";
 
@@ -10,19 +12,19 @@ const WhatIsWT = () => {
 			<h2 className={styles.whatIsWTHeader}>Explore. Rate. Repeat.</h2>
 			<div className={styles.steps}>
 				<HowToStep
-					stepNumber={1}
+					icon={<CgMusicSpeaker />}
 					stepDescription={
 						"Listen to the What's Turning Album of the Day any way you'd like."
 					}
 				/>
 				<HowToStep
-					stepNumber={2}
+					icon={<TfiWrite />}
 					stepDescription={
 						"Click “Create Post” to leave a rating and optional short review."
 					}
 				/>
 				<HowToStep
-					stepNumber={3}
+					icon={<SlGraph />}
 					stepDescription={
 						"Watch your profile grow along with your music taste and knowledge!"
 					}
