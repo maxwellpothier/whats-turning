0c422b703f9d81531c2b585714a844d7cfe56318
maxwellpothier <pothiermaxwell@gmail.com>
2023-07-17 08:34:34 -0600

    adding new domain for images

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	images: {
-		domains: ["upload.wikimedia.org", "i.discogs.com"],
+		domains: ["upload.wikimedia.org", "i.discogs.com", "e.snmc.io"],
 	},
 	env: {
 		BASE_API_URL: process.env.BASE_API_URL,
